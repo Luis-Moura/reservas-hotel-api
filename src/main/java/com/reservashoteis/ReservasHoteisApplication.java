9862b7fa3cf66c1145ccfdb9468267db3a11fde8
Juawo <joaopedrojp2703@gmail.com>
2025-07-25 11:31:39 -0300

    refactor main method to include DAO testing and room retrieval logic

--- src/main/java/com/reservashoteis/ReservasHoteisApplication.java
+++ src/main/java/com/reservashoteis/ReservasHoteisApplication.java
@@ -1,13 +1,27 @@
 package com.reservashoteis;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.reservashoteis.dao.room.RoomDao;
+import com.reservashoteis.model.Room;
 
 @SpringBootApplication
 public class ReservasHoteisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReservasHoteisApplication.class, args);
+        var context = SpringApplication.run(ReservasHoteisApplication.class, args);
+
+        // Teste do DAO
+        RoomDao roomDao = context.getBean(RoomDao.class);
+        List<Room> rooms = roomDao.searchAll();
+        for (Room room : rooms) {
+            // Com o nome dos atributos, get e set diferentes, ele não retorna os dados corretamente.
+            System.out.println("Room number: " + room.getNumber() + "\nRoom type: " + room.getRoomType());
+        }
+        System.exit(0);
     }
 
 }
