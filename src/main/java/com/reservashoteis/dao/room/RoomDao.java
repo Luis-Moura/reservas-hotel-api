4e5a7fb53eb350eab37181416c97272cf30095f5
Juawo <joaopedrojp2703@gmail.com>
2025-07-25 11:31:26 -0300

    implement RoomDao class with searchAll method for retrieving all room records

--- src/main/java/com/reservashoteis/dao/room/RoomDao.java
+++ src/main/java/com/reservashoteis/dao/room/RoomDao.java
@@ -1,4 +1,26 @@
 package com.reservashoteis.dao.room;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.reservashoteis.model.Room;
+
+@Repository
 public class RoomDao {
+    private final JdbcTemplate template;
+
+    @Autowired
+    public RoomDao(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    public List<Room> searchAll() {
+        String sql = "SELECT * FROM quartos";
+        return template.query(sql, new BeanPropertyRowMapper<>(Room.class));
+    }
+
 }
