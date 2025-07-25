530fe2a9649e76708440a98e375c432a6012c84b
Juawo <joaopedrojp2703@gmail.com>
2025-07-25 10:57:38 -0300

    refactor Room class to include getter and setter methods for all properties

--- src/main/java/com/reservashoteis/model/Room.java
+++ src/main/java/com/reservashoteis/model/Room.java
@@ -1,8 +1,34 @@
 package com.reservashoteis.model;
 
 public class Room {
+    
     private int idRoom;
     private int number;
     private String roomType;
     private Float dailyPrice;
+
+    public int getIdRoom() {
+        return idRoom;
+    }
+    public void setIdRoom(int idRoom) {
+        this.idRoom = idRoom;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public String getRoomType() {
+        return roomType;
+    }
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+    public Float getDailyPrice() {
+        return dailyPrice;
+    }
+    public void setDailyPrice(Float dailyPrice) {
+        this.dailyPrice = dailyPrice;
+    }
 }
