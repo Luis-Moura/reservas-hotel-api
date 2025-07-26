13fb136c7f4a069588c77668fb85b1294f7253c4
Luis-Moura <luish.moura2024@gmail.com>
2025-07-26 10:39:15 -0300

    implement ClientDao with CRUD operations using JdbcTemplate

--- src/main/java/com/reservashoteis/dao/client/ClientDaoInterface.java
+++ src/main/java/com/reservashoteis/dao/client/ClientDaoInterface.java
@@ -1,4 +1,18 @@
 package com.reservashoteis.dao.client;
 
+import com.reservashoteis.model.Client;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ClientDaoInterface {
+    public void save(Client client);
+
+    public Optional<Client> findById(Long id);
+
+    public List<Client> findAll();
+
+    public boolean update(Client client);
+
+    public boolean delete(Long id);
 }
