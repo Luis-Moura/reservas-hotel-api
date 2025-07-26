5b3f69ca1f18a5818eee67f9e4383e4e778b91c4
Luis-Moura <luish.moura2024@gmail.com>
2025-07-26 10:44:04 -0300

    remove redundant public modifiers from ClientDaoInterface method declarations

--- src/main/java/com/reservashoteis/dao/client/ClientDaoInterface.java
+++ src/main/java/com/reservashoteis/dao/client/ClientDaoInterface.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ClientDaoInterface {
-    public void save(Client client);
+    void save(Client client);
 
-    public Optional<Client> findById(Long id);
+    Optional<Client> findById(Long id);
 
-    public List<Client> findAll();
+    List<Client> findAll();
 
-    public boolean update(Client client);
+    boolean update(Client client);
 
-    public boolean delete(Long id);
+    boolean delete(Long id);
 }
