e58ec8e62c759bb819e32aa979905d6b4ffc52da
Luis-Moura <luish.moura2024@gmail.com>
2025-07-26 10:39:01 -0300

    add Client class with id, name, email fields and validation logic

--- src/main/java/com/reservashoteis/model/Client.java
+++ src/main/java/com/reservashoteis/model/Client.java
@@ -1,4 +1,33 @@
 package com.reservashoteis.model;
 
 public class Client {
+    private long id;
+    private String name;
+    private String email;
+
+    public Client(long id, String name, String email) {
+        if (name == null || name.isEmpty() || email == null || email.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+
+        if (!email.contains("@")) {
+            throw new IllegalArgumentException("Email must contain '@'");
+        }
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
