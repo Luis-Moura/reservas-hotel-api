aee9ddfda2007d852ed13f5f4f4dd6e01745e4ba
Luis-Moura <luish.moura2024@gmail.com>
2025-07-28 10:42:33 -0300

    add findAllClients, updateClient, and deleteClient endpoints to ClientController

--- src/main/java/com/reservashoteis/controller/ClientController.java
+++ src/main/java/com/reservashoteis/controller/ClientController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController()
 @RequestMapping("/api/client")
 public class ClientController {
@@ -26,5 +28,25 @@ public class ClientController {
         Client response = clientService.findClientById(id);
 
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Client>> findAllClients() {
+        List<Client> response = clientService.findAllClients();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateClient(@PathVariable Long id, @RequestBody ClientRequestDto clientRequestDto) {
+        clientService.updateClient(id, clientRequestDto);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+        clientService.deleteClient(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
