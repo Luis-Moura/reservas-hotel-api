492926174eaa29bcd8e7699a997e97e9d77b0ca4
Luis-Moura <luish.moura2024@gmail.com>
2025-07-28 10:52:08 -0300

    refactor ClientResponseDto to use record for improved conciseness and immutability

--- src/main/java/com/reservashoteis/dto/response/ClientResponseDto.java
+++ src/main/java/com/reservashoteis/dto/response/ClientResponseDto.java
@@ -1,25 +1,4 @@
 package com.reservashoteis.dto.response;
 
-public class ClientResponseDto {
-    private final Long id;
-    private final String name;
-    private final String email;
-
-    public ClientResponseDto(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
+public record ClientResponseDto(Long id, String name, String email) {
 }
