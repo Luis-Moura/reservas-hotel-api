e3e24a06bf2232ffe86ae88a60e6005e71369964
Luis-Moura <luish.moura2024@gmail.com>
2025-07-28 09:47:54 -0300

    add ClientController with createClient endpoint using ClientServiceInterface

--- src/main/java/com/reservashoteis/controller/ClientController.java
+++ src/main/java/com/reservashoteis/controller/ClientController.java
@@ -1,4 +1,21 @@
 package com.reservashoteis.controller;
 
+import com.reservashoteis.dto.request.ClientRequestDto;
+import com.reservashoteis.services.client.ClientServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/api/client")
 public class ClientController {
+    @Autowired
+    private ClientServiceInterface clientService;
+
+    @PostMapping("")
+    public void createClient(@RequestBody ClientRequestDto clientRequestDto) {
+        clientService.createClient(clientRequestDto);
+    }
 }
